b756aa7ac8765927166026dfe4f345c9157b73d1
bhuvneshuchiha <bhuvnesh.awasthi@gmail.com>
2024-07-15 03:30:00 +0530

    Go DAY 3

--- personal_project/main.go
+++ personal_project/main.go
@@ -1,20 +1,18 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "price_calc/prices"
+)
 
 func main() {
 
-    prices := []float64{10, 20 , 30}
-    taxRates := []float64{0, 0.7, 0.1, 0.15}
+    taxRates := []float64{0, 0.07, 0.1, 0.15}
 
     result :=  make(map[float64][]float64)
     for _, taxRate := range taxRates {
-        var taxIncludedPrices []float64 = make([]float64, len(prices))
-        for priceIndex, price := range prices {
-            taxIncludedPrices[priceIndex] = price * (1 + taxRate)
-        }
-        result[taxRate] = taxIncludedPrices
-
+        priceJob := prices.NewTaxIncludedPriceJob(taxRate)
+        priceJob.Process()
     }
     fmt.Println(result)
 
