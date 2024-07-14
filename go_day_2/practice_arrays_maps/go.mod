5fe9aaf1cf9814a2be1ad6b13dee527571cf713d
bhuvneshuchiha <bhuvnesh.awasthi@gmail.com>
2024-07-14 11:09:58 +0530

    GO DAY 3

---
+++ go_day_2/practice_arrays_maps/go.mod
@@ -0,0 +1,3 @@
+module arrays_maps
+
+go 1.22.5
---
+++ go_day_2/practice_arrays_maps/lists.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+
+type Product struct {
+
+    Title string
+    Id string
+    price float64
+}
+
+func main() {
+
+    hobbies := [3]string{"Football", "F1", "Cricket"}
+    fmt.Println(hobbies)
+
+    fmt.Printf("The first element is : %v \n", hobbies[0])
+    fmt.Printf("The second and third element %v \n", hobbies[1:])
+
+    mainHobbies := hobbies[0:2]
+    fmt.Printf("The third task %v \n", mainHobbies)
+
+    mainHobbies = mainHobbies[1:3]
+    fmt.Print(mainHobbies)
+
+    courseGoals := []string{"Be like prime", "Be awesome"}
+    fmt.Println(courseGoals)
+
+    courseGoals[1] = "Be super awesome"
+    courseGoals = append(courseGoals, "Be a master")
+
+    product := []Product{
+
+        {
+            "First Product",
+            "1",
+            12.89,
+    },
+        {
+            "Second Product",
+            "2",
+            22.90,
+        },
+    }
+
+    fmt.Println(product)
+
+
+    newProduct := Product{
+        "A third product",
+        "3",
+        49.99,
+    }
+
+    product = append(product, newProduct)
+
+    discountedArray := []float64{10.9, 12.88}
+    newArray := []float64{1.22, 5.99}
+    newArray = append(newArray, discountedArray...)
+    fmt.Println(newArray)
+
+}
