00dee1f5c58332e5f7efa43a1a3666abdfcc6c76
bhuvneshuchiha <bhuvnesh.awasthi@gmail.com>
2024-07-16 06:56:32 +0530

    GO DAY 4

--- personal_project/cmdmanager/cmdmanager.go
+++ personal_project/cmdmanager/cmdmanager.go
@@ -6,7 +6,7 @@ type CMDManager struct{
 
 }
 
-func (cmd CMDManager) ReadLines([]string, error) {
+func (cmd CMDManager) ReadLines([]string) ([]string, error) {
     fmt.Println("Please enter your prices.Confirm prices with ENTER")
 
     var prices []string
@@ -16,7 +16,7 @@ func (cmd CMDManager) ReadLines([]string, error) {
         fmt.Print("Price: ")
         fmt.Scan(&price)
 
-        if price == 0{
+        if price == "0"{
             break
         }
         prices = append(prices, price)
@@ -24,11 +24,13 @@ func (cmd CMDManager) ReadLines([]string, error) {
     return prices, nil
 }
 
-func (cmd CMDManager) WriteResult(data interface {}, error) {
+func (cmd CMDManager) WriteResult(data interface {}) error {
     fmt.Println(data)
     return nil
 }
 
-
+func New() CMDManager{
+    return CMDManager {}
+}
 
 
