957078d0309d0a568c74768775e0bb3c5a1c5c3a
bhuvneshuchiha <bhuvnesh.awasthi@gmail.com>
2024-08-06 19:50:57 +0530

    terminal_helper

--- terminal_helper/choices/choices.go
+++ terminal_helper/choices/choices.go
@@ -11,7 +11,8 @@ func CreateChoices() {
 	fmt.Println("4- Delete a file.")
 	fmt.Println("5- Move a directory.")
 	fmt.Println("6- Move a file.")
-	fmt.Println("7- Create a TMUX session. (For this you need to have tmux already installed)")
-	fmt.Println("8- Kill a TMUX session. (For this you need to have tmux already installed)")
-	fmt.Println("9- Attach to a tmux session detaching other clients. (For this you need to have tmux already installed)")
+	fmt.Println("7- Kill a process.")
+	fmt.Println("8- Create a TMUX session. (For this you need to have tmux already installed)")
+	fmt.Println("9- Kill a TMUX session. (For this you need to have tmux already installed)")
+	fmt.Println("10- Attach to a tmux session detaching other clients. (For this you need to have tmux already installed)")
 }
--- terminal_helper/processKill/killProcess.go
+++ terminal_helper/processKill/killProcess.go
@@ -1,19 +1,41 @@
 package processkill
 
 import (
+	"bufio"
 	"fmt"
 	"log"
+	"os"
 	"os/exec"
 )
 
 func KillProcess() {
 	fmt.Println("Let me show you the current processes that are running")
 
-	cmd := exec.Command("ps aux | grep", "all")
+	cmd := exec.Command("ps")
 	output, err := cmd.CombinedOutput()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(output)
+	// fmt.Println(reflect.TypeOf(output))
+	fmt.Println(string(output))
+
+	fmt.Println("Enter the PID you wish to kill")
+	var pid string
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		pid = scanner.Text()
+	}
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	com := exec.Command("kill", "-9", pid)
+	_, er := com.CombinedOutput()
+	if er == nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Println("The process has been killed")
 }
