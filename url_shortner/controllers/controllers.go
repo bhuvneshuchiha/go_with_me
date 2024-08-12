aad7c1da7c4dd563687414deebb6e9736a87fd25
bhuvneshuchiha <bhuvnesh.awasthi@gmail.com>
2024-08-13 00:30:56 +0530

    url_shortner

--- url_shortner/controllers/controllers.go
+++ url_shortner/controllers/controllers.go
@@ -1,1 +1,14 @@
 package controllers
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func ReceiveUrl(w http.ResponseWriter, r *http.Request) {
+	fmt.Print("Request received bitches")
+}
+
+func RedirectToMain(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Send the request to the long url")
+}
--- url_shortner/main/main.go
+++ url_shortner/main/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/bhuvneshuchiha/url_shortner/controllers"
+)
+
+func main() {
+	http.HandleFunc("/", controllers.ReceiveUrl)
+	http.HandleFunc("/redirect", controllers.RedirectToMain)
+
+	err := http.ListenAndServe(":3333", nil)
+	if err != nil {
+		log.Fatal("Server went down")
+	}
+}
